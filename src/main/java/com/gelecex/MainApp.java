c0e2467672b30b198718d9022c1ff8b97b2cca64
obetron <erenbasaran@gmail.com>
2019-07-03 01:11:23 +0300

    paths are configured.

--- src/main/java/com/gelecex/MainApp.java
+++ src/main/java/com/gelecex/MainApp.java
@@ -14,7 +14,7 @@ public class MainApp extends Application {
     
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("HashCalculator.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("/HashCalculator.fxml"));
         
         Scene scene = new Scene(root);
         
